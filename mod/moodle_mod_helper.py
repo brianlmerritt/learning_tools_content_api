04f596137420bfb12d351f48903fe3a03c7973c4
Jago Brown <jabrown@rvc.ac.uk>
2025-03-21 17:15:27 +0000

    Additions to help staff weed out large (unused) files in chapters

--- mod/moodle_mod_helper.py
+++ mod/moodle_mod_helper.py
@@ -7,6 +7,8 @@ from urllib.parse import urlparse
 from lib.content_cleaners import content_cleaners
 from lib.content_utilities import content_utilities
 from lib.event_logger import EventLogger
+import math
+from datetime import datetime, timezone
 
 class ModuleHelper:
     """Helper class for processing Moodle module content"""
@@ -195,14 +197,41 @@ class ModuleHelper:
             f'{self.component_name}_files': [],
             f'{self.component_name}_title': content.get('content', ''),
             f'{self.component_name}_filepath': content.get('filepath'),
-            f'{self.component_name}_filesize': content.get('filesize'),
+            f'{self.component_name}_filesize': content.get('filesize'),          
             f'{self.component_name}_fileurl': item_url,
             f'{self.component_name}_time_modified': content.get('timemodified'),
             f'{self.component_name}_sortorder': content.get('sortorder', 0),
             f'{self.component_name}_tags': content.get('tags', [])
         }
         if self.component_name == 'chapter':
+            
             item[f'{self.component_name}_url'] = f"{module_data.get('book_url')}&chapter={item.get('chapter_id')}"
+
+            # Moodle url to text editor where the file manager can be opened and references to unused files deleted.
+            # Mock up url above to test if col & val added to CSV https://learn.rvc.ac.uk/mod/book/edit.php?cmid=58392&id=100115
+            item[f'{self.component_name}_editor_url'] = f"https://learn.rvc.ac.uk/mod/book/edit.php?cmid={int(module_data.get(f'{self.modtype}_cmid'))}&id={item.get('chapter_id')}"
+            # print(item[f'{self.component_name}_editor_url'])
+
+            # Human readable filesize - consider refactoring to use (currently unused) fn below called _convert_size
+            # print("item['chapter_filesize'] = ", item['chapter_filesize'], "type = ", type(item['chapter_filesize']))
+            size_bytes = int(item['chapter_filesize'])
+
+            if size_bytes == 0: 
+                human_size = "0B" 
+            else: 
+                size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB") 
+                i = int(math.floor(math.log(size_bytes, 1024)))
+                power = math.pow(1024, i) 
+                size = round(size_bytes / power, 2) 
+                human_size = "{} {}".format(size, size_name[i])
+
+            item[f'{self.component_name}_filesize_readable'] = f"{human_size}"
+
+            # Human readable time modified
+            ts = int(item['chapter_time_modified'])
+            x = datetime.fromtimestamp(ts, tz=timezone.utc)
+            item[f'{self.component_name}_time_modified_readable'] = x.strftime('%Y-%m-%d %H:%M')
+
         
         
         # Add module data
@@ -267,3 +296,17 @@ class ModuleHelper:
         return self.content_cleaner.clean_encoding_artifacts(
             self.content_cleaner.clean_escaped_slashes(items_with_file_check)
         )
+    
+
+    def _convert_size(size_bytes, ignore2ndposarg): 
+        """
+        original fn taken from https://python-forum.io/thread-6709.html
+        TODO refactor to use a convert size fn here rather than including code within the  _process_item fn
+        """
+        if size_bytes == 0: 
+            return "0B" 
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB") 
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        power = math.pow(1024, i) 
+        size = round(size_bytes / power, 2) 
+        return "{} {}".format(size, size_name[i])
